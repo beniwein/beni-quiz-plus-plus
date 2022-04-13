61f47892142d398a5fe4abeff838141dffa65446
beniwein <beni_weinmann@hotmail.com>
2022-04-13 12:29:10 +0200

    2nd-commit

--- index.php
+++ index.php
@@ -5,22 +5,13 @@ if ($_POST["answer"] == 4) {
 } 
 ?> -->
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="css/style.css"/>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="js/main.js"></script>
-</head>
-<body>
-
 <?php
-    echo '<br> <br>';
+    if (isset($_POST['lastQuestionIndex'])) {
+
+        if (isset ($_POST['nextQuestionIndex'])) {
+            $currentQuestionIndex = $_POST['nextQuestionIndex'];
+        }
+    };
 
     $dbHost = getenv('DB_HOST');
     $dbName = getenv('DB_NAME');
@@ -31,8 +22,31 @@ if ($_POST["answer"] == 4) {
 
     //$query = $dbConnection->query("SELECT * FROM Questions");
     //$query = $dbConnection->query("SELECT Text, Text2 FROM Questions, Answers WHERE ID = 4 AND QuestionId = 4");
-    $query = $dbConnection->query("SELECT Text FROM Questions WHERE ID = 4");
+    //$query = $dbConnection->query("SELECT Text FROM Questions WHERE ID = 4");
     //$query = $dbConnection->query("SELECT Text2 FROM Answers WHERE QuestionId = 4");
+
+    $query = $dbConnection->query("SELECT * FROM Questions");
+    $questions = $query->fetchAll(PDO::FETCH_ASSOC);
+
+    // $query = $dbConnection->query("SELECT Text,Text2 FROM Questions, Answers WHERE IsCorrectAnswer = 1 AND QuestionId = ID");
+    // requires !Foreign-KEY! to match with PRIMARY-KEY *************
+
+    //$query = $dbConnection->query("SELECT Text FROM Answers");
+                             
+    for ($key = 0; $key < count($questions); $key++) {
+        $question = $questions[$key];
+        $subQuery = $dbConnection->prepare("SELECT * FROM Answers WHERE Answers.QuestionId = ?");
+        $subQuery->bindValue(1, $question['ID']);    
+        $subQuery->execute();
+        $answers = $subQuery->fetchAll(PDO::FETCH_ASSOC);
+        $questions[$key]['answers'] = $answers;
+    }
+
+    $_SESSION['quizData'] = $questions;
+
+    echo '<pre>';
+    print_r($_SESSION['quizData']);
+    echo '</pre>';
 
     
     
@@ -41,11 +55,11 @@ if ($_POST["answer"] == 4) {
 
     //$query = $dbConnection->query("SELECT Text FROM Answers");
                              
-    echo '<div class="container-fluid p-5">';
-    echo '<div class="h3">Tech Quiz</div>';
-    echo '<table class="table table-striped">';
-    echo '<thead>';
-    echo '<tr>';
+    //echo '<div class="container-fluid p-5">';
+    //echo '<div class="h3">Tech Quiz</div>';
+    //echo '<table class="table table-striped">';
+    //echo '<thead>';
+    //echo '<tr>';
 
     $columnCount = $query->columnCount();
 
@@ -68,9 +82,9 @@ if ($_POST["answer"] == 4) {
        
     }
     
-    echo '</table>';
-    echo '</div>';
-    echo '</div>';
+    //echo '</table>';
+    //echo '</div>';
+    //echo '</div>';
 
     // echo '<pre>';
     // print_r($query);
@@ -78,16 +92,50 @@ if ($_POST["answer"] == 4) {
 
     ?>
 
-        </div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css"/>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="js/main.js"></script>
+</head>
+<body>
 
-        <form class="form" action="result.php" method="post">
-
+        <form class="form" method="post">
+        <h2>Tech-Quizz</h2>
+        <h3>Question: <?php echo $currentQuestionIndex; ?><h3>
+        <p><?php echo $questions[currentQuestionIndex]['text']; ?></p>
         
         
     <br><button type="submit" class="answer1" name="answer" value="1">Digital wallet</button><br><br>
-        <button type="submit" class="answer2" name="answer" value="2">Mining</button><br><br>
-        <button type="submit" class="answer3" name="answer" value="3">Blockchain</button><br><br>
-        <button type="submit" class="answer4" name="answer" value="4">Token</button>
+        <?php
+            $answers = $questions[$currentQuestionIndex]['answers'];
+            echo $answers[0]['answer'];
+        ?>    
+    <button type="submit" class="answer2" name="answer" value="2">Mining</button><br><br>
+        <?php
+            $answers = $questions[$currentQuestionIndex]['answers'];
+            echo $answers[0]['answer'];
+        ?>
+    <button type="submit" class="answer3" name="answer" value="3">Blockchain</button><br><br>
+    <?php
+            $answers = $questions[$currentQuestionIndex]['answers'];
+            echo $answers[1]['answer'];
+        ?>    
+    <button type="submit" class="answer4" name="answer" value="4">Token</button>
+        <?php
+            $answers = $questions[$currentQuestionIndex]['answers'];
+            echo $answers[0]['answer'];
+        ?>
+
+        <input type="hidden" name="lastQuestionIndex" value="0">
+        <input type="hidden" name="nextQuestionIndex" value="1">
+
 
         </form>
     </main>
