a8716505ad9e743fe095c423a427ba8e447baf5d
beniwein <beni_weinmann@hotmail.com>
2022-04-13 10:13:41 +0200

    1st-commit

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+<!--session_start();
+
+if ($_POST["answer"] == 4) {
+    $_SESSION["score"] += 1;
+} 
+?> -->
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,9 +18,81 @@
     <script src="js/main.js"></script>
 </head>
 <body>
-    <?php
-        echo "Hello, we are starting to work with Databases and PHP PDO!"; 
+
+<?php
+    echo '<br> <br>';
+
+    $dbHost = getenv('DB_HOST');
+    $dbName = getenv('DB_NAME');
+    $dbUser = getenv('DB_USER');
+    $dbPassword = getenv('DB_PASSWORD');
+
+    $dbConnection = new PDO('mysql:host=mysql;dbname=library', 'webDev', 'opport2022');
+
+    //$query = $dbConnection->query("SELECT * FROM Questions");
+    //$query = $dbConnection->query("SELECT Text, Text2 FROM Questions, Answers WHERE ID = 4 AND QuestionId = 4");
+    $query = $dbConnection->query("SELECT Text FROM Questions WHERE ID = 4");
+    //$query = $dbConnection->query("SELECT Text2 FROM Answers WHERE QuestionId = 4");
+
+    
+    
+// $query = $dbConnection->query("SELECT Text,Text2 FROM Questions, Answers WHERE IsCorrectAnswer = 1 AND QuestionId = ID");
+// requires !Foreign-KEY! to match with PRIMARY-KEY *************
+
+    //$query = $dbConnection->query("SELECT Text FROM Answers");
+                             
+    echo '<div class="container-fluid p-5">';
+    echo '<div class="h3">Tech Quiz</div>';
+    echo '<table class="table table-striped">';
+    echo '<thead>';
+    echo '<tr>';
+
+    $columnCount = $query->columnCount();
+
+    for ($i = 0; $i < $columnCount; $i++) {
+        $columnInfo = $query->getColumnMeta($i);
+        $columnName = $columnInfo['name'];
+        echo "<td>$columnName</td>";
+    }
+
+    echo '</tr>';
+    echo '</thead>';
+        
+    while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
+       echo '<tr>';
+       foreach ($row as $columnName => $value) {
+        echo "<td>$value<td>";
+       }
+
+       echo '</tr>';
+       
+    }
+    
+    echo '</table>';
+    echo '</div>';
+    echo '</div>';
+
+    // echo '<pre>';
+    // print_r($query);
+    // echo '</pre>';
+
     ?>
+
+        </div>
+
+        <form class="form" action="result.php" method="post">
+
+        
+        
+    <br><button type="submit" class="answer1" name="answer" value="1">Digital wallet</button><br><br>
+        <button type="submit" class="answer2" name="answer" value="2">Mining</button><br><br>
+        <button type="submit" class="answer3" name="answer" value="3">Blockchain</button><br><br>
+        <button type="submit" class="answer4" name="answer" value="4">Token</button>
+
+        </form>
+    </main>
     
 </body>
+
 </html>
+
---
+++ result.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css"/>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="js/main.js"></script>
+</head>
+<body>
+
+<br><h2>Quiz End</h2>
+<h3>Your Result:</h3>
+
+<?php
+    session_start();
+
+    $_SESSION["page10"] = $_POST["page10"];
+
+    $score = $_SESSION['Correct'];
+
+    if ($score > 7) { 
+        
+            echo  "<h1>You win - thanks for playing...</h1>" . "<br>";
+        
+        }  else { 
+            echo "<h1>You lose - try again...</h1>" . "<br>";
+         
+    }
+
+    print_r ('Score: ' . $score);
+?>
+
+<a class="btn btn-primary btn-sm float-end" href="index.php" type="button">Back to Quiz</a>
+
+<!-- hidden field -->
+<input type="hidden" id="feedback-healthyId" name="feedback-healthyId" value="feedback-healthy">
+
+<script src="main.js"></script>
+</body>
+
+</html>
